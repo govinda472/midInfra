30afe2fe11fe00ddd6f02543da3b36b7ae90b166
Vibu <vibhu987@gmail.com>
2025-02-03 22:33:22 +0530

    Mobile view and Close button changes

--- src/app/pages/learnabout/learnabout.page.ts
+++ src/app/pages/learnabout/learnabout.page.ts
@@ -1,24 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { IonButton, IonIcon, IonContent } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-learnabout',
   templateUrl: './learnabout.page.html',
   styleUrls: ['./learnabout.page.scss'],
   standalone: true,
-  imports: [IonButton, IonIcon, IonContent]
+  imports: [CommonModule, IonButton, IonIcon, IonContent]
 })
 export class LearnaboutPage implements OnInit {
-  constructor(
-    private location: Location
-  ) {}
-
+  constructor(private location: Location) {
+    addIcons({ closeOutline });
+  }
+  
   ngOnInit() {}
-
+  
   closePage() {
-    // This will navigate back while preserving scroll position
     this.location.back();
   }
 }
